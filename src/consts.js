9f4df4d10d9cd6453b032acd9f149978ce2345a3
KinSh <jianchengpan7@gmail.com>
2023-04-08 21:22:04 +0800

    20230408

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `KINSHING Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_EMAIL = 'jianchengpan7@gmail.com'
-export const SITE_NAME = 'kinshingpoon.github.io/blog';
-export const SITE_URL = "https://kinshingpoon.github.io/blog";
+export const SITE_NAME = 'blog-fkk5m9gk8-kinshingpoon.vercel.app';
+export const SITE_URL = "https://blog-fkk5m9gk8-kinshingpoon.vercel.app";
