f0066286d6fb7f658a3dd8784417db03aec7f52a
KinSh <jianchengpan7@gmail.com>
2023-04-09 08:41:54 +0800

    20230409

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `KINSHING Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
+export const SITE_TITLE = `Kin Blog`;
+export const SITE_DESCRIPTION = 'Salted Fish Notes'
 export const SITE_EMAIL = 'jianchengpan7@gmail.com'
-export const SITE_NAME = 'blog-fkk5m9gk8-kinshingpoon.vercel.app';
-export const SITE_URL = "https://blog-fkk5m9gk8-kinshingpoon.vercel.app";
+export const SITE_NAME = 'blog-kinshingpoon.vercel.app';
+export const SITE_URL = "https://blog-kinshingpoon.vercel.app/";
