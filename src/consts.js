6a0d1820d35d89f44cf0d30af3e6837f62636676
kinshingpoon <jianchengpan7@gmail.com>
2023-07-18 23:10:42 +0800

    20230718

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Jack Blog`;
 export const SITE_DESCRIPTION = 'Salted Fish Notes'
-export const SITE_EMAIL = 'jianchengpan7@gmail.com'
+export const SITE_EMAIL = 'jiancheng.pan.plus@gmail.com'
 export const SITE_NAME = 'jianchengpan.space';
 export const SITE_URL = "https://jianchengpan.space/";
