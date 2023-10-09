899e3e017be9e6c6ccf06f0b6d72423cedb56b43
kinshingpoon <jianchengpan7@gmail.com>
2023-10-09 19:45:24 +0800

    add site url

--- src/consts.js
+++ src/consts.js
@@ -2,4 +2,4 @@ export const SITE_TITLE = `EasyBlog`;
 export const SITE_DESCRIPTION = 'Salted Fish Notes'
 export const SITE_EMAIL = 'jiancheng.pan.plus@gmail.com'
 export const SITE_NAME = 'easyblog.tech';
-export const SITE_URL = "https://easyblog.tech/";
+export const SITE_URL = "https://www.easyblog.tech/";
